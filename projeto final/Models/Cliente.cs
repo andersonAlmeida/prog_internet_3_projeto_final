cb96aa7edb84abc8efc6d45cc56c7e01302fe87e
Anderson Almeida <andi.r.almeida@gmail.com>
2019-12-10 22:40:30 -0300

    clientes data types

--- projeto final/Models/Cliente.cs
+++ projeto final/Models/Cliente.cs
@@ -10,11 +10,17 @@ namespace projeto_final.Models
     {
         public int Id { get; set; }
         public string Nome { get; set; }
-        public string Email { get; set; }
-        public string Senha { get; set; }
         public string Sobrenome { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+
         [DataType(DataType.Date)]
         public DateTime Nascimento { get; set; }
+
         public string Cpf { get; set; }
         public string Rg { get; set; }
     }
